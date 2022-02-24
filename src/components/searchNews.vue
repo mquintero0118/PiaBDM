24386eabcc732a7cd0db02da89d072537c94a063
Quintero <m.alejandro.quinter@gmail.com>
2022-02-23 22:30:59 -0600

    First version

--- src/components/searchNews.vue
+++ src/components/searchNews.vue
@@ -1,13 +1,49 @@
 <template>
-  <h2>Hello world Search news</h2>
+  <div class="card cardSearchNews">
+    <div class="card-header">
+      <div class="input-group">
+        <input
+          type="text"
+          class="form-control search"
+          placeholder="¿Que noticia buscas?"
+          aria-label="Input group example"
+          aria-describedby="btnGroupAddon"
+        />
+        <button type="button" class="btn btn-success">
+          <i class="fa fa-search"></i> Buscar
+        </button>
+      </div>
+    </div>
+    <div class="container-fluid">
+<div v-for="index in 3" :key="index" class="card cardSecond" style="width: 10rem">
+    
+          <img src="./testImages/newspaper.png" class="card-img-top" alt="..." />
+          <div class="card-body">
+            <h5 class="card-title">El mejor portal de noticias</h5>
+            <p class="card-text">
+              Archon News fue premiado como el mejor portal para noticias en el mundo...
+            </p>
+            <router-link to="/seeNews" class="btn btn-primary">Ver noticia</router-link>
+          </div>
+        </div>
+    </div>
+        
+    
+    
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
+.cardSearchNews {
+  width: 70%;
+  max-width: 2000px;
+  margin-left: 14%;
+}
+.search {
+  margin-left: 0%;
+}
 </style>
