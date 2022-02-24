67f21fba8167165c0f6245e117a8833ca30eede3
mquintero0118 <m.alejandro.quintero@gmail.com>
2022-02-24 15:05:19 -0600

    Basic skeleton for searchNews

--- src/components/searchNews.vue
+++ src/components/searchNews.vue
@@ -1,6 +1,7 @@
 <template>
-  <div class="card cardSearchNews">
-    <div class="card-header">
+
+  <div class="card text-center cardSearchNews">
+    <div class="card-header ">
       <div class="input-group">
         <input
           type="text"
@@ -14,18 +15,23 @@
         </button>
       </div>
     </div>
-    <div class="container-fluid">
-<div v-for="index in 3" :key="index" class="card cardSecond" style="width: 10rem">
-    
-          <img src="./testImages/newspaper.png" class="card-img-top" alt="..." />
-          <div class="card-body">
-            <h5 class="card-title">El mejor portal de noticias</h5>
-            <p class="card-text">
-              Archon News fue premiado como el mejor portal para noticias en el mundo...
+    <div class="card-body">
+<div v-for="index in 3" :key="index" class="card mb-3 test" style="max-width: 5000px;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="./testImages/newspaper.png" class="img-fluid rounded-start" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Titulo de la noticia</h5>
+        <p class="card-text">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque scelerisque mi eget commodo maximus. Curabitur sed vestibulum libero. Donec lacinia imperdiet purus eget tempus. Donec ut porttitor arcu. Fusce imperdiet, magna quis lacinia semper, felis sapien sagittis risus, id malesuada mauris quam eu nisi. Cras tortor risus, dictum eu congue sed, convallis eu felis. Nullam ultricies efficitur euismod. Vestibulum vel lorem vel orci facilisis fermentum. Integer varius eget ligula ac lacinia. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
             </p>
-            <router-link to="/seeNews" class="btn btn-primary">Ver noticia</router-link>
-          </div>
-        </div>
+         <router-link to="/seeNews" class="btn btn-primary">Ver noticia</router-link>
+      </div>
+    </div>
+  </div>
+</div>
     </div>
         
     
@@ -42,8 +48,19 @@ export default {};
   width: 70%;
   max-width: 2000px;
   margin-left: 14%;
+  margin-bottom: 40px;
 }
 .search {
   margin-left: 0%;
+}
+.form-control::placeholder{
+color: rgba(170, 170, 170, 0.801) !important;
+}
+.cardRelated {
+  margin-left: 35%;
+  margin-bottom: 30px;
+}
+.test {
+  width: 100%;
 }
 </style>
