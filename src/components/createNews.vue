8404ec56fd383aa83a61abb5b17744cd48e53171
Quintero <m.alejandro.quinter@gmail.com>
2022-05-25 23:47:55 -0500

    Changed place arragement

--- src/components/createNews.vue
+++ src/components/createNews.vue
@@ -28,26 +28,30 @@
               <br />
               <div>
                 <label for="text">Texto de la noticia </label>
-                <textarea
-                  type="text"
-                  id="newsText"
-                  class="form-control"
-                />
+                <textarea type="text" id="newsText" class="form-control" />
               </div>
               <br />
-              <div>
-                <label for="place">Lugar de los hechos </label>
-                <input
-                  type="text"
-                  id="newsPlace"
-                  class="form-control"
-                />
+              <div class="row">
+                <div class="d-flex bd-highlight">
+                  <div class=" flex-fill bd-highlight">
+                    <label for="country">Pais </label>
+                    <input type="text" id="newsCountry" class="form-control" />
+                  </div>
+                  <div class=" flex-fill bd-highlight">
+                    <label for="state">Estado </label>
+                    <input type="text" id="newsState" class="form-control" />
+                  </div>
+                  <div class=" flex-fill bd-highlight">
+                    <label for="city">Ciudad </label>
+                    <input type="text" id="newsCity" class="form-control" />
+                  </div>
+                </div>
               </div>
               <br />
               <div>
                 <label for="dataNTime">Fecha y hora </label>
                 <Datepicker v-model="date" showNowButton ref="datepicker">
-                  <template  #am-pm-button="{ toggle, value }">
+                  <template #am-pm-button="{ toggle, value }">
                     <button @click="toggle">{{ value }}</button>
                   </template>
                 </Datepicker>
@@ -135,20 +139,20 @@ import { onMounted, ref } from "vue";
 import Datepicker from "vue3-date-time-picker";
 import "vue3-date-time-picker/dist/main.css";
 import Multiselect from "@vueform/multiselect";
- import axios from "axios";
+import axios from "axios";
 
 export default {
   components: { Datepicker, Multiselect },
 
   setup() {
     /* v-models */
-    
+
     var options = {
-        year: "numeric",
-        month: "2-digit",
-        day: "numeric"
+      year: "numeric",
+      month: "2-digit",
+      day: "numeric",
     };
-    const title = ref('');
+    const title = ref("");
     const section = ref();
     var sections = ref([]);
     const valueTags = ref();
@@ -158,42 +162,53 @@ export default {
     /* Functions */
     function confirm() {
       sendData();
-      
     }
 
+    async function sendData() {
+      var data = new FormData();
+      data.append("title", document.getElementById("newsTitle").value);
+      data.append("lead", document.getElementById("newsLead").value);
+      data.append("text", document.getElementById("newsText").value);
+      data.append("country", document.getElementById("newsCountry").value);
+      data.append("state", document.getElementById("newsState").value);
+      data.append("city", document.getElementById("newsCity").value);
+      data.append("signature", document.getElementById("newsSignature").value);
+      data.append(
+        "video",
+        document.getElementById("formFileMultipleVid").value
+      );
+      data.append("img", document.getElementById("formFileMultipleImg").value);
+      data.append("seccion", section.value);
+      data.append("tag", valueTags.value);
 
-    async function sendData(){
-          var data = new FormData();
-  data.append("title", document.getElementById("newsTitle").value);
-  data.append("lead", document.getElementById("newsLead").value);
-  data.append("text", document.getElementById("newsText").value);
-  data.append("place", document.getElementById("newsPlace").value);
-  data.append("signature", document.getElementById("newsSignature").value);
-  data.append("video", document.getElementById("formFileMultipleVid").value);
-  data.append("img", document.getElementById("formFileMultipleImg").value);
-  data.append("seccion", section.value);
-  data.append("tag", valueTags.value);
+      var datelocal = date.value.toLocaleDateString("en", options);
+      var dateSQL =
+        datelocal.substring(6) +
+        "-" +
+        datelocal.substring(0, 2) +
+        "-" +
+        datelocal.substring(3, 5);
 
-var datelocal = date.value.toLocaleDateString("en",options);
-var dateSQL = datelocal.substring(6) + "-" + datelocal.substring(0,2) + "-" + datelocal.substring(3,5);
+      data.append(
+        "dateTime",
+        dateSQL + " " + date.value.toTimeString().split(" ")[0]
+      );
+      //console.log( date.value.toJSON().slice(0, 19).replace('T', ' '));
 
-  data.append("dateTime", dateSQL+' '+date.value.toTimeString().split(' ')[0]);
-  //console.log( date.value.toJSON().slice(0, 19).replace('T', ' '));
-  
-//       data.append("lastName", lastNameV.value)
-//       data.append("email", emailV.value);
-//       data.append("pass", passwordV.value);
-     
-for(var pair of data.entries()) {
-    console.log(pair[0]+ ', '+ pair[1]); 
-}
+      //       data.append("lastName", lastNameV.value)
+      //       data.append("email", emailV.value);
+      //       data.append("pass", passwordV.value);
 
-await axios
+      for (var pair of data.entries()) {
+        console.log(pair[0] + ", " + pair[1]);
+      }
+
+      await axios
         .post(
           //http://localhost:8070/test.php?action=create
           // http://localhost:8070/piaBDMBack/api.php?action=create
-          "http://localhost/pia_BDM/piaBDMBack/includes/news_inc.php?action=create",
-         //"http://localhost/PIA_BDM/piaBDMBack/api.php?action=create",
+          "http://localhost:8070/piaBDMBack/piaBDMBack/includes/news_inc.php?action=create",
+          //"http://localhost/PIA_BDM/piaBDMBack/api.php?action=create",
           data,
           {
             headers: {
@@ -204,40 +219,32 @@ await axios
         .then((res) => {
           if (res.data.error == true) {
             console.log("Ocurrio un error!", res.data);
-          //  setTimeout(() => {
-            
-         //   }, 1000);
+            //  setTimeout(() => {
+
+            //   }, 1000);
           } else {
-           // setTimeout(() => {
-            
-             
-         //   }, 2000);
+            // setTimeout(() => {
+            //   }, 2000);
           }
         })
         .catch((error) => {
           console.log("Ocurrio un error en el servicio", error);
-       //   setTimeout(() => {
-          
-          
-        //  }, 2000);
+          //   setTimeout(() => {
+
+          //  }, 2000);
         });
     }
 
-
-
-  
-
-
-    onMounted(()=>{
+    onMounted(() => {
       /* Aqui haces la peticion con axios */
       /* response lo iguales a una constante
       tipo asi sections = res.data.data*/
       /* es necesario que tenga una estructura como la de arriba*/
 
-        axios
+      axios
         .get(
-          "http://localhost/pia_BDM/piaBDMBack/includes/section_inc.php?action=selectSections",
-         //"http://localhost/PIA_BDM/piaBDMBack/api.php?action=create",
+          "http://localhost:8070/piaBDMBack/piaBDMBack/includes/section_inc.php?action=selectSections",
+          //"http://localhost/PIA_BDM/piaBDMBack/api.php?action=create",
           null,
           {
             headers: {
@@ -248,35 +255,30 @@ await axios
         .then((res) => {
           if (res.data.error == true) {
             console.log("Ocurrio un error!", res.data);
-          //  setTimeout(() => {
-          
+            //  setTimeout(() => {
 
-
-        //    }, 1000);
+            //    }, 1000);
           } else {
-        //    setTimeout(() => {
-              console.log(res.data[0]);
+            //    setTimeout(() => {
+            console.log(res.data[0]);
 
-              res.data[0].forEach(element => {
-               sections.value.push(element["DESCRIPTION"]);
-               console.log(element);
-              });
+            res.data[0].forEach((element) => {
+              sections.value.push(element["DESCRIPTION"]);
+              console.log(element);
+            });
 
-           //  sections.value = [res.data[0][0].DESCRIPTION, res.data[0][1].DESCRIPTION, res.data[0][2].DESCRIPTION];             
+            //  sections.value = [res.data[0][0].DESCRIPTION, res.data[0][1].DESCRIPTION, res.data[0][2].DESCRIPTION];
             console.log(sections);
-        //    }, 2000);
+            //    }, 2000);
           }
         })
         .catch((error) => {
           console.log("Ocurrio un error en el servicio", error);
-       //   setTimeout(() => {
-          
-        //  }, 2000);
+          //   setTimeout(() => {
+
+          //  }, 2000);
         });
-
-
-
-    })
+    });
     return {
       /* v-models */
       title,
